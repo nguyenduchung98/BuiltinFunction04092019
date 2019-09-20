39f1ad43e0c077424a6c3cd60c562bdfd7396281
Nguyen Duc Hung <hungrom98x@gmail.com>
2019-09-20 20:41:27 +0700

    Phuong thuc co ban

--- app/src/main/java/nguyenduchung/ndh/builtinfunction04092019/MainActivity.java
+++ app/src/main/java/nguyenduchung/ndh/builtinfunction04092019/MainActivity.java
@@ -13,8 +13,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         // method == phương thức
         // Giả quyết vấn đè lặp code
-       int ketqua= tinhTong(3,4);
-        Log.d("DDD",ketqua+"");
+        //in so chan
+        //inSoChan();
+        //in so le
+        //inSoLe();
+        //in so chia 3 du 1
+        insoChi3du1();
     }
     // Thường đc tạo trong class
     // 1: Access modifier : phạm vi truy cập
@@ -25,5 +29,35 @@ public class MainActivity extends AppCompatActivity {
     private Integer tinhTong(int a,int b){
         int tong= a + b;
         return tong;
+    }
+    private void inSoChan(){
+        for (int i=0;i<=100;i++){
+            if(i==0){
+                continue;
+            }
+            else if(i%2==0){
+                Log.d("DDD",i+ " ");
+            }
+        }
+    }
+    private void inSoLe(){
+        for (int i=0;i<=100;i++){
+            if(i==0){
+                continue;
+            }
+            else if(i%2!=0){
+                Log.d("DDD",i+ " ");
+            }
+        }
+    }
+    private void insoChi3du1(){
+        for (int i=0;i<=100;i++){
+            if(i==0){
+                continue;
+            }
+            else if(i%3==1){
+                Log.d("DDD",i+ " ");
+            }
+        }
     }
 }
