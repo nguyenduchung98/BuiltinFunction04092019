14c0399831557c363be5cc367336206bb37fc182
Nguyen Duc Hung <hungrom98x@gmail.com>
2019-09-20 20:20:36 +0700

    Function: Phuong thuc

--- app/src/main/java/nguyenduchung/ndh/builtinfunction04092019/MainActivity.java
+++ app/src/main/java/nguyenduchung/ndh/builtinfunction04092019/MainActivity.java
@@ -3,6 +3,7 @@ package nguyenduchung.ndh.builtinfunction04092019;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +13,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         // method == phương thức
         // Giả quyết vấn đè lặp code
-
+       int ketqua= tinhTong(3,4);
+        Log.d("DDD",ketqua+"");
+    }
+    // Thường đc tạo trong class
+    // 1: Access modifier : phạm vi truy cập
+        // public : ở đâu cung thấy đc
+        //private :tạo ở đâu thì dùng ở đó
+    // 2: Giá trị trả về
+    // 3: Tên phương thức :
+    private Integer tinhTong(int a,int b){
+        int tong= a + b;
+        return tong;
     }
 }
